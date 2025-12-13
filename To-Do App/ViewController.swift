bce3f55904914734c1180dfb374a60a9ccff5403
Konstantin <kkodolov@icloud.com>
2025-12-13 16:35:27 +0500

    Initial Commit

---
+++ To-Do App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  To-Do App
+//
+//  Created by Костя Кодолов on 13.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ To-Do AppTests/To_Do_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_AppTests.swift
+//  To-Do AppTests
+//
+//  Created by Костя Кодолов on 13.12.2025.
+//
+
+import Testing
+@testable import To_Do_App
+
+struct To_Do_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
